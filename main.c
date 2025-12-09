e3aef60912c4509ab2ace7a85e3139bb69d0bded
LM-Estudiante05 <luis.cabrera.figueroa@udla.edu.ec>
2025-12-09 11:34:42 -0500

    main.c

--- main.c
+++ main.c
@@ -3,44 +3,38 @@
 
 int main() {
     Libro libros[10];
-    int cont = 0;
+    Usuario usuarios[10];
+    int contLib = 0;
+    int contUsu = 0;
     int opc;
 
     do {
         opc = menu();
 
         switch (opc) {
-            case 1:
-                registrarLibro(libros, &cont);
-                break;
+            case 1: registrarLibro(libros, &contLib); break;
+            case 2: listarLibros(libros, contLib); break;
+            case 3: buscarLibro(libros, contLib); break;
+            case 4: eliminarLibro(libros, &contLib); break;
 
-            case 2:
-                listarLibros(libros, cont);
-                break;
+            case 5: registrarUsuario(usuarios, &contUsu); break;
+            case 6: listarUsuarios(usuarios, contUsu); break;
+            case 7: buscarUsuario(usuarios, contUsu, libros, contLib); break;
 
-            case 3:
-                buscarLibro(libros, cont);
-                break;
+            case 8: rentarLibro(libros, contLib, usuarios, contUsu, contLib); break;
+            case 9: devolverLibro(libros, contLib, usuarios, contUsu, contLib); break;
 
-            case 4:
-                actualizarEstado(libros, cont);
-                break;
+            case 10: editarLibro(libros, contLib); break;
 
-            case 5:
-                eliminarLibro(libros, &cont);
-                break;
-
-            case 6:
+            case 11:
                 printf("Saliendo del programa...\n");
                 break;
 
             default:
-                printf("Opcion invalida. Intente nuevamente.\n");
+                printf("Opcion invalida.\n");
         }
 
-        printf("\n");
-        
-    } while (opc != 6);
+    } while (opc != 11);
 
     return 0;
 }
