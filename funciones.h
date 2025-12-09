cd51bd361d98021411d1d3b7eeb9c560f0d69487
LM-Estudiante05 <luis.cabrera.figueroa@udla.edu.ec>
2025-12-09 11:34:08 -0500

    funciones.h

--- funciones.h
+++ funciones.h
@@ -7,19 +7,36 @@ typedef struct {
     char titulo[100];
     char autor[50];
     int anio;
-    char estado[20];   
+    char estado[20];
+    char estadoFisico[20];
 } Libro;
 
+typedef struct {
+    int id;
+    char nombre[50];
+    int idLibroRentado;   
+} Usuario;
 
+/* ----- Utilidades ----- */
 void leerCadena(char *cadena, int n);
 int intPositivo();
 int idUnico(Libro libros[], int cont, int id);
 int menu();
 
+/* ----- Funciones Libro ----- */
 void registrarLibro(Libro libros[], int *cont);
 void listarLibros(Libro libros[], int cont);
 int buscarLibro(Libro libros[], int cont);
-void actualizarEstado(Libro libros[], int cont);
 void eliminarLibro(Libro libros[], int *cont);
+void editarLibro(Libro libros[], int cont);
+
+/* ----- Funciones Usuario ----- */
+void registrarUsuario(Usuario usuarios[], int *contU);
+void listarUsuarios(Usuario usuarios[], int contU);
+int buscarUsuario(Usuario usuarios[], int contU, Libro libros[], int contL);
+
+/* ----- Préstamos ----- */
+void rentarLibro(Libro libros[], int contL, Usuario usuarios[], int contU, int cont);
+void devolverLibro(Libro libros[], int contL, Usuario usuarios[], int contU, int cont);
 
 #endif
